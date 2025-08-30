f89258cea32e7c42ab715696f2d101fea7ddbc4b
GitRen25 <renatonomussp@gmail.com>
2025-08-30 22:18:19 +0000

    Pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Luana e Miguel este é o resultado dos meus estudos, que tal um sorriso se
+      achar isso massa !
+    </h1>
+  );
+}
+
+export default Home;
