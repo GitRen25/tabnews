a9bdb8f7d1fc50b657a6d35d9fb0d9dd4b05c957
GitRen25 <renatonomussp@gmail.com>
2025-08-31 12:11:20 +0000

    alteração da frase

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Luana e Miguel este é o resultado dos meus estudos, que tal um sorriso se
-      achar isso massa !
+      achar isso massa ! Amo vocês !
     </h1>
   );
 }
